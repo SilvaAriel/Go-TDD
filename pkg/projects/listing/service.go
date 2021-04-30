ee7c21c624d2b4d65177e983c628f715a0c8942c
Ariel Silva <ariel120292@gmail.com>
2021-04-30 08:19:19 -0300

    Start TestListing

--- pkg/projects/listing/service.go
+++ pkg/projects/listing/service.go
@@ -1,1 +1,17 @@
 package listing
+
+type Repository interface {
+	ListAllProjects() []Project
+}
+
+type Service struct {
+	r Repository
+}
+
+func (s *Service) ListAllProjects() []Project {
+	return s.r.ListAllProjects()
+}
+
+func NewService(r Repository) *Service {
+	return &Service{r}
+}
--- pkg/projects/listing/service_test.go
+++ pkg/projects/listing/service_test.go
@@ -1,7 +1,34 @@
 package listing
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+	"time"
+)
 
 func TestListing(t *testing.T) {
+	now := time.Now
 
+	t.Run("List all projects", func(t *testing.T) {
+		mockrepo := new(mockRepo)
+		mockrepo.Projects = []Project{{"Destroy E-Corp", now()}}
+
+		s := NewService(mockrepo)
+
+		got := s.ListAllProjects()
+		want := []Project{
+			{"Destroy E-Corp", now()},
+		}
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("Got %s, but wanted %s", got, want)
+		}
+	})
+}
+
+type mockRepo struct {
+	Projects []Project
+}
+
+func (m *mockRepo) ListAllProjects() []Project {
+	return m.Projects
 }
