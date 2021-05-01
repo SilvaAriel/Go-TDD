464b9099ac92228ad957e163b6ada8f4888e9116
Ariel Silva <ariel120292@gmail.com>
2021-05-01 15:43:01 -0300

    Initial files

--- pkg/projects/listing/service_test.go
+++ pkg/projects/listing/service_test.go
@@ -1,12 +1,13 @@
 package listing
 
 import (
+	"errors"
 	"reflect"
 	"testing"
 	"time"
 )
 
-func TestListing(t *testing.T) {
+func TestListing_ListAll(t *testing.T) {
 	now := time.Now
 
 	t.Run("List all projects", func(t *testing.T) {
@@ -24,29 +25,55 @@ func TestListing(t *testing.T) {
 			t.Errorf("Got %v, but wanted %v", got, want)
 		}
 	})
-	t.Run("Get project by id", func(t *testing.T) {
-		mockrepo := new(mockRepo)
-		project := Project{1, "Destroy E-Corp", now()}
-		mockrepo.Projects = []Project{project}
+}
 
-		s := NewService(mockrepo)
+func TestListing_GetById(t *testing.T) {
+	now := time.Now
 
-		got := s.GetById(project.ID)
-		want := Project{1, "Destroy E-Corp", now()}
-		if !reflect.DeepEqual(got, want) {
-			t.Errorf("Got %v, but wanted %v", got, want)
-		}
-	})
+	tt := []struct {
+		id            int
+		mockProject   []Project
+		want          Project
+		expectedError error
+	}{
+		{
+			id:          1,
+			mockProject: []Project{{1, "Destroy E-Corp", now()}},
+			want:        Project{1, "Destroy E-Corp", now()},
+		},
+		{
+			id:            2,
+			mockProject:   []Project{{1, "Destroy E-Corp", now()}},
+			want:          Project{},
+			expectedError: errors.New("Id not found"),
+		},
+	}
+
+	for _, tc := range tt {
+		t.Run("Get project by id", func(t *testing.T) {
+			mockrepo := new(mockRepo)
+			project := Project{1, "Destroy E-Corp", now()}
+			mockrepo.Projects = tc.mockProject
+
+			s := NewService(mockrepo)
+
+			got, err := s.GetById(project.ID)
+			if err != tc.expectedError {
+				t.Errorf("Got %v, but wanted %v", got, tc.want)
+			}
+		})
+	}
+
 }
 
 type mockRepo struct {
-	Projects []Project
+	ExpectedProject []Project
 }
 
 func (m *mockRepo) ListAll() []Project {
-	return m.Projects
+	return m.ExpectedProject
 }
 
-func (m *mockRepo) GetById(id int) Project {
-	return m.Projects[0]
+func (m *mockRepo) GetById(id int) (Project, error) {
+	return m.expectedProject, m.expectedError
 }
