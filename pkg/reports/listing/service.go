89d30f3bc8fe12055e992806f66f876ab0250242
Ariel Silva <ariel120292@gmail.com>
2021-05-17 08:34:44 -0300

    Create TestListingAll

--- pkg/reports/listing/service.go
+++ pkg/reports/listing/service.go
@@ -1,15 +1,20 @@
 package listing
 
 type Repository interface {
-	GetById(projectId int, reportId int) (*Report, error)
+	GetById(projectId int, reportId int) ([]Report, error)
+	GetAll(projectId int) ([]Report, error)
 }
 
 type service struct {
 	r Repository
 }
 
-func (s *service) GetById(projectId int, reportId int) (*Report, error) {
+func (s *service) GetById(projectId int, reportId int) ([]Report, error) {
 	return s.r.GetById(projectId, reportId)
+}
+
+func (s *service) GetAll(projectId int) ([]Report, error) {
+	return s.r.GetAll(projectId)
 }
 
 func NewService(r Repository) *service {
--- pkg/reports/listing/service_test.go
+++ pkg/reports/listing/service_test.go
@@ -7,16 +7,16 @@ import (
 	"time"
 )
 
-func TestListing(t *testing.T) {
+func TestListingByID(t *testing.T) {
 	now := time.Time{}
 	tt := map[string]struct {
 		mockrepo *mockRepo
 	}{
-		"List by ID":                {&mockRepo{1, 6, &Report{1, 6, "Teste", now, true}, nil}},
-		"Expect correct Project ID": {&mockRepo{1, 6, &Report{1, 6, "Teste", now, true}, nil}},
-		"Expect correct Report ID":  {&mockRepo{2, 7, &Report{2, 7, "Teste", now, true}, nil}},
-		"Invalid Project ID":        {&mockRepo{5, 1, nil, errors.New("")}},
-		"Invalid Report ID":         {&mockRepo{1, 5, nil, errors.New("")}},
+		"List by Project and Report ID": {&mockRepo{10, 16, []Report{{10, 16, "Teste", now, true}}, nil}},
+		"Expect correct Project ID":     {&mockRepo{1, 6, []Report{{1, 6, "Teste", now, true}}, nil}},
+		"Expect correct Report ID":      {&mockRepo{2, 7, []Report{{2, 7, "Teste", now, true}}, nil}},
+		"Invalid Project ID":            {&mockRepo{5, 1, nil, errors.New("")}},
+		"Invalid Report ID":             {&mockRepo{1, 5, nil, errors.New("")}},
 	}
 
 	for name, tc := range tt {
@@ -25,12 +25,12 @@ func TestListing(t *testing.T) {
 			report, err := s.GetById(tc.mockrepo.ProjectId, tc.mockrepo.ReportId)
 
 			if tc.mockrepo.Output != nil {
-				if tc.mockrepo.ProjectId != tc.mockrepo.Output.ProjectID {
-					t.Errorf("Got Project ID %d, but expected %d", tc.mockrepo.Output.ProjectID, tc.mockrepo.ProjectId)
+				if tc.mockrepo.ProjectId != tc.mockrepo.Output[0].ProjectID {
+					t.Errorf("Got Project ID %d, but expected %d", tc.mockrepo.Output[0].ProjectID, tc.mockrepo.ProjectId)
 				}
 
-				if tc.mockrepo.ReportId != tc.mockrepo.Output.ID {
-					t.Errorf("Got Report ID %d, but expected %d", tc.mockrepo.Output.ProjectID, tc.mockrepo.ProjectId)
+				if tc.mockrepo.ReportId != tc.mockrepo.Output[0].ID {
+					t.Errorf("Got Report ID %d, but expected %d", tc.mockrepo.Output[0].ProjectID, tc.mockrepo.ProjectId)
 				}
 			}
 
@@ -44,13 +44,42 @@ func TestListing(t *testing.T) {
 	}
 }
 
+func TestListingAll(t *testing.T) {
+	now := time.Time{}
+	tt := map[string]struct {
+		mockrepo *mockRepo
+	}{
+		"List by Project ID": {&mockRepo{0, 10, []Report{{1, 10, "Teste", now, true}, {2, 10, "Teste", now, true}, {3, 10, "Teste", now, false}}, nil}},
+	}
+
+	for name, tc := range tt {
+		t.Run(name, func(t *testing.T) {
+			s := NewService(tc.mockrepo)
+			report, err := s.GetAll(tc.mockrepo.ProjectId)
+
+			if !reflect.DeepEqual(report, tc.mockrepo.Output) {
+				t.Errorf("Got %v, but expected %v", err, tc.mockrepo.Output)
+			}
+			for _, report := range tc.mockrepo.Output {
+				if tc.mockrepo.ProjectId != report.ProjectID {
+					t.Errorf("Got project ID %d, but expected %d", report.ProjectID, tc.mockrepo.ProjectId)
+				}
+			}
+		})
+	}
+}
+
 type mockRepo struct {
 	ReportId      int
 	ProjectId     int
-	Output        *Report
+	Output        []Report
 	ExpectedError error
 }
 
-func (m *mockRepo) GetById(projectId int, reportId int) (*Report, error) {
+func (m *mockRepo) GetById(projectId int, reportId int) ([]Report, error) {
+	return m.Output, m.ExpectedError
+}
+
+func (m *mockRepo) GetAll(projectId int) ([]Report, error) {
 	return m.Output, m.ExpectedError
 }
